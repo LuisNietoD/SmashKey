41227f998524a7d58f2e331f10c7fdbf36af877b
LuisNietoD <107162215+LuisNietoD@users.noreply.github.com>
2024-12-07 18:28:21 +0100

    Enemy

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -10,3 +10,11 @@ public interface IWeapon
     public int damage { get; }
     public void AutoShoot();
 }
+
+public interface IEnemy
+{
+    public int damage { get; }
+    public int health { get; set; }
+    public void Attack();
+    public void Hit(int damageAmount);
+}
