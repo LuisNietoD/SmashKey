a2baf0860d20ccab8ecd5f2744fb79ddb8c4173d
ZircliX <lolofy71@gmail.com>
2024-12-07 20:52:01 +0100

    Added Round to floats

--- Assets/Scripts/Stats/Structs/PlayerStats.cs
+++ Assets/Scripts/Stats/Structs/PlayerStats.cs
@@ -6,6 +6,6 @@ public struct PlayerStats
     public int totalKilled;
     public float totalDamageDealt;
     public int totalBulletsShot;
-    public Dictionary<IWeapon, int> bulletsPerWeapon;
+    public Dictionary<IWeapon, int> weaponsShot;
     public int totalKeysTapped;
 }
