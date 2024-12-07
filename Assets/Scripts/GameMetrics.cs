26289ca8dace62ccee581a9fbd7f7369606ab550
ZircliX <lolofy71@gmail.com>
2024-12-07 19:18:54 +0100

    Fixed Player + metrics

--- Assets/Scripts/GameMetrics.cs
+++ Assets/Scripts/GameMetrics.cs
@@ -5,5 +5,5 @@ public class GameMetrics : ScriptableObject
 {
     public static GameMetrics Global => GameController.Metrics;
     
-    [field : SerializeField] public float platformTravelTime { get; private set; } = 10f;
+    [field : SerializeField] public float platformTravelTime { get; private set; } = -10f;
 }
