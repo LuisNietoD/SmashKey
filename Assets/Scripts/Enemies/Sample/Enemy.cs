f6247d2a7dfe51c97bff0bc0bf22ef231bb5ec35
LuisNietoD <107162215+LuisNietoD@users.noreply.github.com>
2024-12-08 13:39:12 +0100

    FX package

---
+++ Assets/Scripts/Enemies/Sample/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Splines;
+
+public class Enemy : MonoBehaviour
+{
+    private SplineContainer splineContainer;
+    private float movementDuration;
+    private float timeElapsed = 0f;
+
+    public void Initialize(SplineContainer spline, float duration)
+    {
+        splineContainer = spline;
+        movementDuration = duration;
+    }
+
+    void Update()
+    {
+        if (splineContainer != null)
+        {
+            timeElapsed += Time.deltaTime;
+            float t = Mathf.Clamp01(timeElapsed / movementDuration);
+            transform.position = splineContainer.EvaluatePosition(t);
+            if (timeElapsed / movementDuration > 1)
+            {
+                timeElapsed = 0;
+            }
+        }
+    }
+}
